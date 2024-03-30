e731ea2a52e7c7dd280fa843ec17ce7f1d2290a7
Himanshu Shekhar <96346919+HimanshuShekhar18@users.noreply.github.com>
2024-03-31 02:02:29 +0530

    Update 1_Sliding_Window_Maximum.cpp

--- SLINDING WINDOW/1_Sliding_Window_Maximum.cpp
+++ SLINDING WINDOW/1_Sliding_Window_Maximum.cpp
@@ -1,1 +1,26 @@
 
+vector<int> Solution::slidingMaximum(const vector<int> &A, int B) {
+    
+    int n = A.size();
+        if(n == 0)
+            return {};
+        deque<int> deq;
+
+        vector<int> result;
+        
+        for(int i = 0; i<n; i++) {
+            // 1) remove the max elements from front which are out of window size
+            while(!deq.empty() && deq.front() <= i-B)
+            deq.pop_front();
+            
+            // 2) we maintain the deque in descending order
+            while(!deq.empty() && A[i] > A[deq.back()])
+            deq.pop_back();
+            
+            deq.push_back(i);
+
+            if(i >= B-1) // 3) Only when the window size first gets equal or greater than k
+            result.push_back(A[deq.front()]); // 4) front will have the max element (dequeue is maintained in descending order)
+        }
+        return result;
+}
